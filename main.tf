fc5f0d0c7c49a9467142f777205fbcba9eac17e4
Rajesh333942 <rajeshct.bit@gmail.com>
2023-06-08 14:51:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,29 @@
-module "TfRemote" {
-  source = "github.com/Rajesh333942/TfRemote"
-  container_groups    = var.container_groups
-  resource_group_name = var.resource_group_name
-  location            = var.location
-  Repo_URL= var.Repo_URL
+resource "azurerm_resource_group" "example" {
+  name     = "tfdemo"
+  location = "East US"
+}
+
+# Loop through container groups
+resource "azurerm_container_group" "container_group" {
+  for_each = var.container_groups
+
+  name                = each.value.name
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  os_type             = each.value.os_type
+
+  container {
+    name   = each.value.name
+    image  = each.value.image
+    cpu    = each.value.cpu
+    memory = each.value.memory
+    
+
+    ports {
+      protocol = each.value.port_protocol
+      port     = each.value.port
+    }
+
+    environment_variables = each.value.environment_variables
+  }
 }
